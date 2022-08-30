bab07d020fcd0789343a7fa7415ddb5d1b7181c3
Jaffar Hussein <jaffhussein@icloud.com>
2022-08-30 10:00:13 +0200

    Creates the document layout and configures tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightOrange: 'rgba(255, 103, 0, 1)',
+        paleGrey: 'rgba(235, 235, 235, 1)',
+        darkGrey: 'rgba(192, 192, 192, 1)',
+        paleBlue: 'rgba(58, 110, 165, 1)',
+        darkBlue: 'rgba(0, 78, 152, 1)'
+      }
+    },
+  },
+  plugins: [],
+}
