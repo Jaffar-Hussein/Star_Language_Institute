cc1d12498a15588a67c22a96c97441aad983c1b3
Jaffar Hussein <jaffhussein@icloud.com>
2022-08-31 19:45:41 +0200

    Adds the contact form and responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    container: {
+      center: true,
+      // padding: '2rem',
+    },
     screens: {
       sm: '480px',
       md: '768px',
@@ -9,14 +13,21 @@ module.exports = {
       xl: '1440px'
     },
     extend: {
+      backgroundImage: {
+        'lady': "url('./assets/lady.svg')",
+        
+      },
       colors: {
         brightOrange: 'rgba(255, 103, 0, 1)',
         paleGrey: 'rgba(235, 235, 235, 1)',
         darkGrey: 'rgba(192, 192, 192, 1)',
         paleBlue: 'rgba(58, 110, 165, 1)',
-        darkBlue: 'rgba(0, 78, 152, 1)'
+        darkBlue: 'rgba(0, 78, 152, 1)',
+        lightGrey: 'rgba(240, 240, 240, 1)'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
