5bb437752df2e3b4003b534a3097d75f21241e55
Jaffar Hussein <jaffhussein@icloud.com>
2022-09-03 20:20:29 +0200

    Deployment 2.0

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ const quote = document.getElementById("quote")
 const author = document.getElementById("author")
 const quotes_url = "https://type.fit/api/quotes"
 const assets = "https://starinstitute.herokuapp.com/assets"
+const about = "https://starinstitute.herokuapp.com/about"
+const courses = "https://starinstitute.herokuapp.com/all_courses"
 const hero_desktop = document.getElementById("hero_desktop")
 const hero_mobile = document.getElementById("hero_mobile")
 const hero_svg1 = document.getElementById("hero_svg1")
@@ -13,6 +15,28 @@ const hero_svg5 = document.getElementById("hero_svg5")
 const about_mobile = document.getElementById("about_mobile")
 const about_desktop = document.getElementById("about_desktop")
 const logo = document.getElementById("logo");
+const about_founder = document.getElementById("about_founder")
+const about_institute = document.getElementById("about_institute")
+const founder_name = document.getElementById("founder_name")
+const primary_phoneNumber = document.getElementById("primary_phoneNumber")
+const secondary_phoneNumber = document.getElementById("secondary_phoneNumber")
+const contact_email = document.getElementById("contact_email")
+const tag_line = document.getElementById("tag_line")
+const description_1 = document.getElementById("1_description")
+const description_2 = document.getElementById("2_description")
+const description_3 = document.getElementById("3_description")
+const name_1 = document.getElementById("1_name")
+const name_2 = document.getElementById("2_name")
+const name_3 = document.getElementById("3_name")
+const duration_1 = document.getElementById("1_duration")
+const duration_2 = document.getElementById("2_duration")
+const duration_3 = document.getElementById("3_duration")
+const price_1 = document.getElementById("1_price")
+const price_2 = document.getElementById("2_price")
+const price_3 = document.getElementById("3_price")
+const image_1 = document.getElementById("1_image")
+const image_2 = document.getElementById("2_image")
+const image_3 = document.getElementById("3_image")
 // Consuming the quotes API
 
 // Consuming custom API 
@@ -21,7 +45,7 @@ setTimeout(() => {
     fetch(quotes_url)
         .then((response) => response.json())
         .then(data => {
-            quote.textContent =  data[0].text
+            quote.textContent = data[0].text
             author.textContent = data[0].author
             // console.log(data[0]);
             // const quote = data[0][random]
@@ -39,6 +63,45 @@ imageAPIConsumption(hero_svg4, "hero_svg4")
 imageAPIConsumption(hero_svg5, "hero_svg5")
 imageAPIConsumption(about_desktop, "about_desktop")
 
+
+// 
+
+const form = document.getElementById("address-form");
+
+// Consuming text content
+aboutAPIconsumption(about_institute, "institute_description");
+aboutAPIconsumption(about_founder, "about_founder");
+aboutAPIconsumption(founder_name, "founder_name");
+aboutAPIconsumption(primary_phoneNumber, "primary_phoneNumber");
+aboutAPIconsumption(secondary_phoneNumber, "secondary_phoneNumber");
+aboutAPIconsumption(contact_email, "contact_email");
+aboutAPIconsumption(tag_line, "tag_line");
+
+// Consuming courses data
+
+coursesAPIconsumption(name_1,description_1,price_1,duration_1,image_1,1)
+
+function aboutAPIconsumption(node, name) {
+    fetch(about)
+        .then(resp => resp.json())
+        .then(data => {
+            node.textContent = data[0][name]
+        })
+}
+
+function coursesAPIconsumption(node_name, node_description, node_price, node_duration, node_course_Image, id) {
+    fetch(courses)
+        .then(resp => resp.json())
+        .then(data => {
+            // node_name.textContent = data[id]
+            node_description.textContent = data[id]["description"]
+            node_name.textContent = data[id]["name"]
+            node_price.textContent = data[id]["price"]
+            node_course_Image.src = cloudinary + data[id]["course_Image"]
+            node_duration.textContent = data[id]["duration"]
+        })
+}
+
 function imageAPIConsumption(node, name) {
     fetch(assets)
         .then(resp => resp.json())
@@ -46,7 +109,3 @@ function imageAPIConsumption(node, name) {
             node.src = cloudinary + data[0][name]
         })
 }
-
-// 
-
-const form = document.getElementById("address-form");
